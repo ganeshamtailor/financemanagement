43999fabc6b0d0c193ce73cc533a53967ee03883
Ganesham <ganeshamtailor@gmail.com>
2023-06-08 16:33:31 +0530

    some changes in investment controller

--- controllers/investment.js
+++ controllers/investment.js
@@ -3,8 +3,8 @@ const Investment = require('../models/investment');
 exports.getInvestment = async (req, res) => {
   try {
     const { id } = req.params;
-
-    const investment = await Investment.findById(id);
+    const userId = req.user.id;
+    const investment = await Investment.findOne({ _id: id, userId });
     if (!investment) {
       return res.status(404).json({ message: 'Investment not found' });
     }
@@ -17,7 +17,8 @@ exports.getInvestment = async (req, res) => {
 
 exports.getAllInvestments = async (req, res) => {
   try {
-    const investments = await Investment.find();
+    const userId = req.user.id;
+    const investments = await Investment.find({userId});
     res.json(investments);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -25,8 +26,8 @@ exports.getAllInvestments = async (req, res) => {
 };
 
 exports.createInvestment = async (req, res) => {
-  const { name, amount, type, userId, startDate, endDate, description } = req.body;
-
+  const { name, amount, type, startDate, endDate, description } = req.body;
+  const userId = req.user.id;
   try {
     const investment = new Investment({
       name,
@@ -50,8 +51,8 @@ exports.updateInvestment = async (req, res) => {
   try {
     const { id } = req.params;
     const { name, amount, type, startDate, endDate, description } = req.body;
-
-    const investment = await Investment.findById(id);
+    const userId = req.user.id;
+    const investment = await Investment.findOne({ _id: id, userId });;
     if (!investment) {
       return res.status(404).json({ message: 'Investment not found' });
     }
@@ -73,8 +74,8 @@ exports.updateInvestment = async (req, res) => {
 exports.deleteInvestment = async (req, res) => {
   try {
     const { id } = req.params;
-
-    const investment = await Investment.findById(id);
+    const userId = req.user.id;
+    const investment = await Investment.findOne({ _id: id, userId });
     if (!investment) {
       return res.status(404).json({ message: 'Investment not found' });
     }
